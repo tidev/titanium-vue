2347f2cc4823a67921156bfb87b0dcbfe7717ffc
Jan Vennemann <jan.vennemann@gmx.net>
2017-07-20 23:38:08 -0700

    Indent using tabs

--- platform/titanium/vdom/VirtualDomNode.js
+++ platform/titanium/vdom/VirtualDomNode.js
@@ -23,60 +23,60 @@ export default class VirtualDomNode {
 	static NODE_TYPE_COMMENT = 8;
 	static NODE_TYPE_DOCUMENT = 9;
 
-  /**
-   * Appends the child node to this vdom node
-   *
-   * The base implementation if this only creates the parent/child and sibling
-   * relations.
-   *
-   * @param {VirtualDomNode} childNode The child node to add
-   */
+	/**
+	 * Appends the child node to this vdom node
+	 *
+	 * The base implementation if this only creates the parent/child and sibling
+	 * relations.
+	 *
+	 * @param {VirtualDomNode} childNode The child node to add
+	 */
 	appendChild(childNode) {
 		if (!(childNode instanceof VirtualDomNode)) {
-      throw new TypeError(`Can only add other virtual dom nodes as child`);
-    }
+			throw new TypeError(`Can only add other virtual dom nodes as child`);
+		}
 
-    if (childNode.parentNode) {
-      throw new Error(`Can't append child because it already has a parent.`);
-    }
+		if (childNode.parentNode) {
+			throw new Error(`Can't append child because it already has a parent.`);
+		}
 
 		childNode.parentNode = this;
 		this.children.push(childNode);
 
-    if (this.lastChild) {
-      childNode.prevSibling = this.lastChild
-      this.lastChild.nextSibling = childNode
-    }
+		if (this.lastChild) {
+			childNode.prevSibling = this.lastChild
+			this.lastChild.nextSibling = childNode
+		}
 	}
 
-  /**
-   * Removes a child node from this vnode
-   *
-   * @param {VirtualDomNode} childNode The child node to remove
-   */
+	/**
+	 * Removes a child node from this vnode
+	 *
+	 * @param {VirtualDomNode} childNode The child node to remove
+	 */
 	removeChild(childNode) {
 		if (!(childNode instanceof VirtualDomNode)) {
-      throw new TypeError(`Can only remove other virtual dom nodes`);
-    }
+			throw new TypeError(`Can only remove other virtual dom nodes`);
+		}
 
-    if (!childNode.parentNode) {
-      throw new Error(`Can't remove child because it has no parent.`)
-    }
+		if (!childNode.parentNode) {
+			throw new Error(`Can't remove child because it has no parent.`)
+		}
 
-    if (childNode.parentNode !== this) {
-      throw new Error(`Can't remove child because it has a different parent.`)
-    }
+		if (childNode.parentNode !== this) {
+			throw new Error(`Can't remove child because it has a different parent.`)
+		}
 
 		if (childNode.prevSibling) {
-      childNode.prevSibling.nextSibling = childNode.nextSibling
-    }
+			childNode.prevSibling.nextSibling = childNode.nextSibling
+		}
 
-    if (childNode.nextSibling) {
-      childNode.nextSibling.prevSibling = childNode.prevSibling
-    }
+		if (childNode.nextSibling) {
+			childNode.nextSibling.prevSibling = childNode.prevSibling
+		}
 
-    childNode.parentNode = null
-    this.children = this.children.filter(node => node !== childNode)
+		childNode.parentNode = null
+		this.children = this.children.filter(node => node !== childNode)
 	}
 
 	toString() {
