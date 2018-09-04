36a6c68a2bd0da86f5d852e32ce6fb166c292e04
Jan Vennemann <jan.vennemann@gmx.net>
2018-09-04 20:46:29 +0200

    fix(tab-group): make sure to insert tabs at correct index
    ... and also to remove them if the tab components gets destroyed

--- platform/titanium/runtime/components/tab/tab.js
+++ platform/titanium/runtime/components/tab/tab.js
@@ -1,5 +1,5 @@
 export default {
-	name: 'tab',
+	name: 'Tab',
 	inject: [ 'getTabGroup' ],
 	render(h) {
 		return h('titanium-tab', this.$slots.default);
@@ -10,8 +10,19 @@ export default {
 			throw new Error('A Tab view must contain a single window as its only child');
 		}
 
-		let tabView = this.$titaniumView;
+		const tabView = this.$titaniumView;
 		tabView.window = window;
-		this.getTabGroup().addTab(tabView);
+		const tabIndex = this.$parent.$children.indexOf(this);
+		if (this.getTabGroup().tabs) {
+			const tabs = this.getTabGroup().tabs;
+			tabs.splice(tabIndex, 0, tabView);
+			this.getTabGroup().tabs = tabs;
+		} else {
+			this.getTabGroup().addTab(tabView);
+		}
+	},
+	destroyed() {
+		const tabView = this.$titaniumView;
+		this.getTabGroup().removeTab(tabView);
 	}
 };
