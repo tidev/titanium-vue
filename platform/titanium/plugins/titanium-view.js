e39e0c363f1d61b0b6de9b4b9dea8f1efb4f337b
Jan Vennemann <jan.vennemann@gmx.net>
2018-09-17 22:42:13 +0200

    fix: check if component root is a Titanium view

--- platform/titanium/plugins/titanium-view.js
+++ platform/titanium/plugins/titanium-view.js
@@ -1,7 +1,13 @@
+import { TitaniumElement } from 'titanium-vdom';
+
 export default {
 	install(Vue) {
 		Object.defineProperty(Vue.prototype, '$titaniumView', {
 			get() {
+				if (!(this.$el instanceof TitaniumElement)) {
+					throw new Error(`The root element ${this.el.tagName} of component ${this.$options.name} is no known Titanium view.`);
+				}
+
 				return this.$el.titaniumView;
 			}
 		});
